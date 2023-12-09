3390161ce54cb8924942743127f18ba9a3979f44
shekinah <nyamburamary1212@gmail.com>
2023-12-09 12:28:02 +0300

    Creating a node

---
+++ CSF/__tests__/bst-node.test.js
@@ -0,0 +1,11 @@
+// __tests__/bst-node.test.js
+import BSTNode from '../src/bst-node.js';
+
+describe('bstNode', () => {
+  test('should correctly create a node', () => {
+    const node = new BSTNode(36);
+    expect(node.data).toEqual(36);
+    expect(node.left).toEqual(null);
+    expect(node.right).toEqual(null);
+  });
+});
---
+++ CSF/src/bst-node.js
@@ -0,0 +1,9 @@
+// src/bst-node.js
+export default class BSTNode {
+    constructor(data) {
+      this.data = data;
+      this.left = null;
+      this.right = null;
+    }
+  }
+  
